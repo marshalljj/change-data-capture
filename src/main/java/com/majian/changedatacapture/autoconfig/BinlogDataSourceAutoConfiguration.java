d32b74ec9eea14f2f7334f28ec2c60fea0d54ce0
马坚 <majian@u51.com>
2018-06-13 20:22:55 +0800

    datasource auto config

--- src/main/java/com/majian/changedatacapture/autoconfig/BinlogDataSourceAutoConfiguration.java
+++ src/main/java/com/majian/changedatacapture/autoconfig/BinlogDataSourceAutoConfiguration.java
@@ -3,23 +3,34 @@ package com.majian.changedatacapture.autoconfig;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import sun.tools.java.ClassType;
 
 @Configuration
 @ConditionalOnMissingBean(name = "binlogDataSource")
 public class BinlogDataSourceAutoConfiguration {
 
-    @Value("${binlog.datasource.key}")
-    public String configKey;
-
-    @Value("${binlog.datasource.tag}")
-    public String configTag;
+    @Value("${binlog.datasource.url}")
+    public String url;
+    @Value("${binlog.datasource.username}")
+    public String userName;
+    @Value("${binlog.datasource.password}")
+    public String password;
+    @Value("${binlog.datasource.driver}")
+    public String driver;
 
 
     @Bean
-    public DataSource binlogDataSource() {
-        throw new RuntimeException("此处还为实现");
+    public DataSource binlogDataSource() throws ClassNotFoundException {
+        Class type = Class.forName("com.zaxxer.hikari.HikariDataSource");
+        return DataSourceBuilder.create()
+            .type(type)
+            .url(url)
+            .driverClassName(driver)
+            .username(userName).password(password)
+            .build();
     }
 
 }
